f2ff6501595605986163c4e8db73eac418542820
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-05-30 11:18:21 +0200

    Adding book names to js file

---
+++ JavaScript2/Week_02/app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+{
+  const bookTitles = [
+    // Replace with your own book titles
+    "harry_potter_chamber_secrets",
+    "the_sun_also_rises_ernest_hemingway",
+    "number_the_stars_lois_lowry",
+    "brave_new_world_aldous_huxley",
+    "remembrance_of_things_past_marcel_proust",
+    "the_fault_in_our_stars_john_green",
+    "cold_comfort_farm_stella_gibbons",
+    "mortal_engines_philip_reeve",
+    "the_dark_tower_stephen_king",
+    "gone_with_the_wind_margaret_mitchell",
+  ];
+
+  // Replace with your own code
+
+}
+
