f0cdb20751f770b533407d97b2a6b2c230b8bf13
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-06-02 20:24:25 +0200

    Syntax update

--- JavaScript2/Week_02/app.js
+++ JavaScript2/Week_02/app.js
@@ -68,57 +68,56 @@
   };
 
   const bookImg = {
-    harry_potter_chamber_secret: "./images/Harry Potter.png",
-    the_sun_also_rises: "./images/The-sun-also-rises.png",
-    number_the_stars: "./images/Number-the-stars.png",
-    brave_new_world: "./images/Brave-New-World.png",
-    remembrance_of_things_past: "./images/Remembrance of Things Past.png",
-    the_fault_in_our_stars: "./images/The-Fault-in-Our-Stars.png",
-    cold_comfort_farm: "./images/Cold-Comfort-Farm.png",
-    mortal_engines: "./images/Mortal-Engines.png",
-    the_dark_tower: "./images/The-Dark-Tower.png",
-    gone_with_the_wind: "./images/Gone-with-the-Wind.png",
-  }
-
+    harry_potter_chamber_secret: './images/Harry Potter.png',
+    the_sun_also_rises: './images/The-sun-also-rises.png',
+    number_the_stars: './images/Number-the-stars.png',
+    brave_new_world: './images/Brave-New-World.png',
+    remembrance_of_things_past: './images/Remembrance of Things Past.png',
+    the_fault_in_our_stars: './images/The-Fault-in-Our-Stars.png',
+    cold_comfort_farm: './images/Cold-Comfort-Farm.png',
+    mortal_engines: './images/Mortal-Engines.png',
+    the_dark_tower: './images/The-Dark-Tower.png',
+    gone_with_the_wind: './images/Gone-with-the-Wind.png',
+  };
 
   const container = document.createElement('div');
-  container.setAttribute("class", "wrapper");
+  container.setAttribute('class', 'wrapper');
   const unorderedList = document.createElement('ul');
-  const title = document.createElement("h1");
-  document.body.append(title)
-  title.innerText = "Top 10 books to read";
+  const title = document.createElement('h1');
+  document.body.append(title);
+  title.innerText = 'Top 10 books to read';
 
   function bookGenerator() {
     const bookKeys = Object.keys(bookInfo);
 
     for (let i = 0; i < bookKeys.length; i++) {
-      const myDiv = document.createElement("div")
-      myDiv.setAttribute("class", "moreInfo")
+      const myDiv = document.createElement('div');
+      myDiv.setAttribute('class', 'moreInfo');
 
-      const divWrapper = document.createElement("div")
-      divWrapper.setAttribute("class", "divWrapper")
+      const divWrapper = document.createElement('div');
+      divWrapper.setAttribute('class', 'divWrapper');
 
       const listItem = document.createElement('li');
       const titles = document.createElement('h2');
       const author = document.createElement('h3');
       const language = document.createElement('h4');
       const images = document.createElement('img');
-      const btn = document.createElement("button");
+      const btn = document.createElement('button');
 
       const book = bookInfo[bookKeys[i]];
       const bookImage = bookImg[bookKeys[i]];
 
       titles.append(book.title);
-      author.append("Author: " + book.author);
-      language.append("Language: " + book.language);
+      author.append('Author: ' + book.author);
+      language.append('Language: ' + book.language);
       // images.append(bookImage)
-      images.src = bookImage
+      images.src = bookImage;
 
-      btn.innerText = "More info"
+      btn.innerText = 'More info';
 
       // Appending images and myDiv inside divWrapper
-      divWrapper.appendChild(myDiv)
-      divWrapper.appendChild(images)
+      divWrapper.appendChild(myDiv);
+      divWrapper.appendChild(images);
 
       // Appending elements
       document.body.appendChild(container);
@@ -131,20 +130,16 @@
       myDiv.appendChild(author);
       myDiv.appendChild(language);
 
-      myDiv.style.display = "none";
+      myDiv.style.display = 'none';
 
-      btn.addEventListener("click", function () {
-        if (myDiv.style.display === "block") {
-          myDiv.style.display = "none";
+      btn.addEventListener('click', function () {
+        if (myDiv.style.display === 'block') {
+          myDiv.style.display = 'none';
+        } else {
+          myDiv.style.display = 'block';
         }
-        else {
-          myDiv.style.display = "block";
-        }
-      })
-
+      });
     }
-
-
   }
   bookGenerator();
 }
