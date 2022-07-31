e62f7be8086cff5500de7ae2494ea4f494795437
Yuliia Panchuk <jula1panchuk@gmail.com>
2022-07-31 10:55:57 +0200

    Update

--- Portfolio/React/src/api/account.js
+++ Portfolio/React/src/api/account.js
@@ -1,11 +1,9 @@
-import { getAuth, signInWithEmailAndPassword } from "./firebase";
+import { getAuth, signInWithEmailAndPassword } from './firebase';
 
 export function isSignedIn() {
   const auth = getAuth();
   return Boolean(auth.currentUser);
 }
-
-// To do: create Log out btn and (onClick{signOut} and redirect to home page)
 export function signOut() {
   const auth = getAuth();
   auth.signOut();
@@ -18,14 +16,8 @@ export async function signInWithPassword(email, password) {
     // https://firebase.google.com/docs/auth/web/start#web-version-9_2
     await signInWithEmailAndPassword(auth, email, password);
     return true;
-  }
-  catch (error) {
+  } catch (error) {
     // user invalid credentials
     return false;
   }
-
-  // TODO: Save the response somewhere, so we can access it when sending the other requests.
-  // TODO: Create a function to remove that variable, as a sign out function
-  // TODO: Create a function that helps to check if the user is signed in
-  // TODO: Return a response so we know if the user signed in
 }
