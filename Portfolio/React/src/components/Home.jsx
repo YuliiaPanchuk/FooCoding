0841e5fba11d57f4bd74da28b19de3a97630c239
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-08-26 21:49:16 +0200

    Adding more content to Home section

--- Portfolio/React/src/components/Home.jsx
+++ Portfolio/React/src/components/Home.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import { Navbar } from './Navbar';
 
-export function Home () {
+export function Home() {
   return (
     <div id="home">
       <Navbar />
 
-      <div className="container">
-        <p className="nameText">YULIIA PANCHUK</p>
-        <p className="devText">
-          FRONT-END <br /> DEVELOPER
-        </p>
-      </div>
+      <section className="homeContainer">
+        <div className="homeSectionCenter">
+          <h1>Hi, my name is</h1>
+        </div>
+
+        <div className="homeSectionCenter">
+          <h2 className="bigHeader">Julia Panchuk</h2>
+        </div>
+
+        <div className="homeSectionCenter">
+          <h3 className="bigHeader">
+            I am passionate Full stack developer with focus on Front end.
+          </h3>
+        </div>
+
+        <div className="homeSectionCenter">
+          <p>
+            I am looking for an opportunity to enhance my skills, offering solid competence and
+            value to the team while developing personal growth.
+          </p>
+        </div>
+        <div className="contactLink">
+          <a
+            className="contactLink"
+            href="https://www.linkedin.com/in/julia-panchuk/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Contact me
+          </a>
+        </div>
+      </section>
     </div>
   );
-};
+}
