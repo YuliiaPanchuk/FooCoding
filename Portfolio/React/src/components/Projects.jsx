09542ba7f00d8e65e6367eff4d3991bd5c2ddd06
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-08-26 21:49:32 +0200

    Update Project content

--- Portfolio/React/src/components/Projects.jsx
+++ Portfolio/React/src/components/Projects.jsx
@@ -15,18 +15,39 @@ export function Projects() {
   }, []);
 
   return (
-    <div id="projects">
-      {projects.map((projectItem) => (
-        <div className="wraper" key={projectItem.id}>
-          <a href={projectItem.link}>
-            <img className="projectImage" src={projectItem.image_url} alt="" />
-          </a>
-          <div className="wrapperText">
-            <p className="headline">{projectItem.name}</p>
-            <p className="description">{projectItem.description}</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <section id="projects">
+      <h2 className="numberedHeader">My projects</h2>
+      <div className="projectsWrapper">
+        {projects.map((projectItem) => (
+          <ul className="wraper" key={projectItem.id}>
+            <li className="listProjects">
+
+              <div className="projectContent">
+                <h3 className="headline">{projectItem.name}</h3>
+                <p className="description">{projectItem.description}</p>
+
+                <ul className="projectSkillsLink">
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>JavaScript</li>
+                </ul>
+
+                <div className="projectSource">
+                  <a href="/GITHUB_PAGE" rel="noreferrer"></a>
+                  <a href="/GITHUB_PAGE" rel="noreferrer"></a>
+                </div>
+
+              </div>
+
+              <div className="projectImage">
+                <a href={projectItem.link}>
+                  <img className="projectImage" src={projectItem.image_url} alt="" />
+                </a>
+              </div>
+            </li>
+          </ul>
+        ))}
+      </div>
+    </section>
   );
-};
+}
